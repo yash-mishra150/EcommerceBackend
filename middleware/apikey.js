e592b4e72e8f3f168e586dbb0fac9f20fd41e997
Yash Mishra <praisedboy5000@gmail.com>
2024-10-28 13:59:46 +0530

    Some Changes related to auth

--- middleware/apikey.js
+++ middleware/apikey.js
@@ -2,11 +2,13 @@ const API_KEY = process.env.API_KEY; // Store your API key in .env file
 
 const apiKeyMiddleware = (req, res, next) => {
     const apiKey = req.header('api_key');
-
-    if (!apiKey || apiKey !== API_KEY) {
-        return res.status(403).json({ msg: 'Forbidden: Invalid API Key' });
+    if (!apiKey) {
+        return res.status(403).json({ msg: 'No Api Key Found' });
     }
 
+    if (apiKey !== API_KEY) {
+        return res.status(403).json({ msg: 'Invalid API Key' });
+    }
     next();
 };
 
