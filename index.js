a707cba5b22e61bc40a29e02349959804c8f969b
Yash Mishra <praisedboy5000@gmail.com>
2024-11-16 20:34:06 +0530

    temp testing changes

--- index.js
+++ index.js
@@ -15,54 +15,44 @@ const PORT = process.env.PORT || 3000;
 const cors = require('cors');
 app.set('trust proxy', 1);
 
-
 const corsOptions = {
   origin: '*',
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   credentials: true,
   optionsSuccessStatus: 204
 };
-app.use('*', cors(corsOptions));
-
-
-
+app.use(cors(corsOptions));
 
 const EventEmitter = require('events');
 const bus = new EventEmitter();
-
-app.use(bodyParser.json());
-
-app.use(bodyParser.urlencoded({ extended: true }));
-
 bus.setMaxListeners(20);
+
+// Middleware setup
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(apiKeyMiddleware);
-app.use(limiter);
+// app.use(limiter);
 app.use(removeWhitespace);
 
-
-
-app.use(bodyParser.json());
-
-
-
-
-
+// Route setup
 app.use('/api/auth', auth);
 app.use('/api/upload', UPP);
 app.use('/api/eVerify', emailVerify);
 app.use('/api/product', products);
 app.use('/api/cart', cart);
+
+// Basic route
 app.get('/', (req, res) => {
   res.send('Welcome to my Node.js backend!');
 });
 
-
+// Server setup
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
 
+// MongoDB connection
 const mongoURI = process.env.MONGODB_URI;
-
 mongoose.connect(mongoURI)
   .then(() => console.log('MongoDB connected successfully'))
   .catch((err) => console.log('MongoDB connection error: ', err));
