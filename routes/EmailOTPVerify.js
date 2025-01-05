a817a0a5354a5dd00585b17fd935f21883beb1f5
yash-mishra150 <praisedboy5000@gmail.com>
2025-01-05 19:33:09 +0530

    added contact us route

--- routes/EmailOTPVerify.js
+++ routes/EmailOTPVerify.js
@@ -1,51 +1,51 @@
-const express = require('express');
-const nodemailer = require('nodemailer');
-const crypto = require('crypto');
-const User = require('../schema/auth/userSchema');
-const OtpVerification = require('../schema/auth/otpVerification');
-require('dotenv').config();
+const express = require("express");
+const nodemailer = require("nodemailer");
+const crypto = require("crypto");
+const User = require("../schema/auth/userSchema");
+const OtpVerification = require("../schema/auth/otpVerification");
+require("dotenv").config();
 const router = express.Router();
 
-router.post('/send-otp', async (req, res) => {
-    const { email } = req.body;
+router.post("/send-otp", async (req, res) => {
+  const { email } = req.body;
 
-    try {
-        // Check if the user already exists
-        let user = await User.findOne({ email });
-        if (user) {
-            return res.status(400).json({ message: 'User already verified.' });
-        }
+  try {
+    // Check if the user already exists
+    let user = await User.findOne({ email });
+    if (user) {
+      return res.status(400).json({ message: "User already verified." });
+    }
 
-        // Delete expired OTP entries
-        await OtpVerification.deleteMany({ email, otpExpiry: { $lt: Date.now() } });
+    // Delete expired OTP entries
+    await OtpVerification.deleteMany({ email, otpExpiry: { $lt: Date.now() } });
 
-        // Generate and hash OTP
-        const otp = Math.floor(100000 + Math.random() * 900000).toString();
-        const otpHash = crypto.createHash('sha256').update(otp).digest('hex');
-        const otpExpiry = Date.now() + 5 * 60 * 1000; // 5 minutes in milliseconds
+    // Generate and hash OTP
+    const otp = Math.floor(100000 + Math.random() * 900000).toString();
+    const otpHash = crypto.createHash("sha256").update(otp).digest("hex");
+    const otpExpiry = Date.now() + 5 * 60 * 1000; // 5 minutes in milliseconds
 
-        // Store OTP hash and expiry in the database
-        await OtpVerification.create({
-            email,
-            otpHash,
-            otpExpiry
-        });
+    // Store OTP hash and expiry in the database
+    await OtpVerification.create({
+      email,
+      otpHash,
+      otpExpiry,
+    });
 
-        // Configure nodemailer transporter
-        const transporter = nodemailer.createTransport({
-            service: 'gmail',
-            auth: {
-                user: process.env.EMAIL,
-                pass: process.env.EMAIL_PASSWORD
-            }
-        });
+    // Configure nodemailer transporter
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: process.env.EMAIL,
+        pass: process.env.EMAIL_PASSWORD,
+      },
+    });
 
-        // Email options
-        const mailOptions = {
-            from: process.env.EMAIL,
-            to: email,
-            subject: 'Your OTP for Food Delivery Verification',
-            text: `
+    // Email options
+    const mailOptions = {
+      from: process.env.EMAIL,
+      to: email,
+      subject: "Your OTP for Food Delivery Verification",
+      text: `
 Dear Customer,
         
 Thank you for choosing GrabEats!
@@ -68,63 +68,107 @@ GrabEats
 Tushar Bansal,
 Co-Founder
 GrabEats
-            `
-        };
+            `,
+    };
 
-        // Send OTP email
-        await transporter.sendMail(mailOptions);
-        return res.status(200).json({ message: 'OTP sent successfully' });
-
-    } catch (error) {
-        return res.status(500).json({ message: 'An error occurred', error: error.message });
-    }
+    // Send OTP email
+    await transporter.sendMail(mailOptions);
+    return res.status(200).json({ message: "OTP sent successfully" });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ message: "An error occurred", error: error.message });
+  }
 });
 
-router.post('/verify-otp', async (req, res) => {
-    const { email, otp } = req.body;
+router.post("/send-email", async (req, res) => {
+  const { email, name, message } = req.body;
 
-    try {
+  try {
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: process.env.EMAIL_TWO,
+        pass: process.env.EMAIL_TWO_PASS,
+      },
+    });
 
-        const otpHash = crypto.createHash('sha256').update(otp).digest('hex');
+    const mailOptions = {
+      from: process.env.EMAIL_TWO,
+      to: email,
+      subject: "Thank You for Reaching Out",
+      text: `
+Dear ${name},
 
+Thank you for reaching out through my portfolio.
 
-        const otpRecord = await OtpVerification.findOne({ email: email });
+For your reference, here is a copy of the message you sent:
 
-        if (!otpRecord) {
-            return res.status(400).json({ message: 'No OTP record found. Please request a new OTP.' });
-        }
+Message:
+${message}
 
-        if (otpRecord.otpExpiry < Date.now()) {
-            await OtpVerification.deleteOne({ email: email });
-            return res.status(400).json({ message: 'OTP expired. Please request a new OTP.' });
-        }
+I look forward to connecting with you and will follow up soon.
 
+Best regards,
+Yash Mishra
+`,
+    };
 
-        if (otpRecord.otpHash !== otpHash) {
-            return res.status(400).json({ message: 'Invalid OTP' });
-        }
+    await transporter.sendMail(mailOptions);
+    return res.status(200).json({ message: "message sent successfully" });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ message: "An error occurred", error: error.message });
+  }
+});
 
+router.post("/verify-otp", async (req, res) => {
+  const { email, otp } = req.body;
 
-        let user = await User.findOne({ email: email });
-        if (user) {
-            if (user.isVerified) {
-                await OtpVerification.deleteOne({ email: email }); 
-                return res.status(400).json({ message: 'User already verified.' });
-            }
-        } else {
+  try {
+    const otpHash = crypto.createHash("sha256").update(otp).digest("hex");
 
-            user = new User({ email, isVerified: true });
-            await user.save();
-        }
+    const otpRecord = await OtpVerification.findOne({ email: email });
 
+    if (!otpRecord) {
+      return res
+        .status(400)
+        .json({ message: "No OTP record found. Please request a new OTP." });
+    }
 
+    if (otpRecord.otpExpiry < Date.now()) {
+      await OtpVerification.deleteOne({ email: email });
+      return res
+        .status(400)
+        .json({ message: "OTP expired. Please request a new OTP." });
+    }
+
+    if (otpRecord.otpHash !== otpHash) {
+      return res.status(400).json({ message: "Invalid OTP" });
+    }
+
+    let user = await User.findOne({ email: email });
+    if (user) {
+      if (user.isVerified) {
         await OtpVerification.deleteOne({ email: email });
-
-        return res.status(200).json({ message: 'OTP verified successfully, user verified.' });
-    } catch (error) {
-        return res.status(500).json({ message: 'An error occurred', error: error.message });
+        return res.status(400).json({ message: "User already verified." });
+      }
+    } else {
+      user = new User({ email, isVerified: true });
+      await user.save();
     }
-});
 
+    await OtpVerification.deleteOne({ email: email });
+
+    return res
+      .status(200)
+      .json({ message: "OTP verified successfully, user verified." });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ message: "An error occurred", error: error.message });
+  }
+});
 
 module.exports = router;
